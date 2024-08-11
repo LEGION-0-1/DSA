fcc17e914d38824d238d8c6fcbd82badf5ec3923
LEGION-0-1 <the.dilpreet.1@gmail.com>
2024-08-11 19:48:59 +0530

    Completed the bubble sort program with separate function for sorting

--- bubble.c
+++ bubble.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+int bubble(int num[], int count){
+    for(int a = 0; a < count; a++){
+        for(int i = 0; i < count; i++){
+        if(num[i]>num[i+1]){
+            int temp = num[i];
+            num[i] = num[i+1];
+            num[i+1] = temp;
+        }
+    }
+    }
+    printf("\nThe sorted array is: ");
+    for(int i = 0; i < count; i++){
+        printf("%d ", num[i]);
+    }
+}
+
 int main(){
     int a,count;
     printf("Please enter the number of inputs: ");
@@ -9,7 +25,9 @@ int main(){
     for(int i = 0; i < count; i++){
         scanf("%d", &num[i]);
     }
+    printf("The original array is: ");
     for(int i = 0; i < count; i++){
         printf("%d ", num[i]);
     }
+    bubble(num, count);
 }
