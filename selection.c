dea3dca151413a2f65f93171f6ee7eb9b59c9ed9
LEGION-0-1 <the.dilpreet.1@gmail.com>
2024-08-13 20:26:36 +0530

    Selection sort program complete

--- selection.c
+++ selection.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+int selection(int num[], int count){
+    for(int i = 0; i < count; i++){
+        int position = i;
+        for(int j = 0; j < count; j++){
+            if (num[position] > num[j]){
+                position = j;
+            }if(position != i){
+                int swap = num[i];
+                num[i] = num[position];
+                num[position] = swap;
+            }
+        }
+    }
+    printf("\nThe sorted array is: ");
+    for(int i = 0; i < count; i++){
+        printf("%d ", num[i]);
+    }
+}
+
 int main(){
     int a,count;
     printf("Please enter the number of inputs: ");
