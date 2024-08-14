8a9331de0d75bb65cc5a04390f4943be1e117a66
LEGION-0-1 <the.dilpreet.1@gmail.com>
2024-08-14 21:32:43 +0530

    Shell sort program done

--- shell.c
+++ shell.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+void shell(int num[], int count) {
+    for (int interval = count / 2; interval > 0; interval /= 2) {
+        for (int i = interval; i < count; i += 1) {
+            int temp = num[i];
+            int j;
+            for (j = i; j >= interval && num[j - interval] > temp; j -= interval) {
+                num[j] = num[j - interval];
+            }
+            num[j] = temp;
+        }
+    }
+    printf("\nThe sorted array is: ");
+    for (int i = 0; i < count; ++i) {
+        printf("%d ", num[i]);
+    }
+    printf("\n");
+}
+
 int main(){
     int a,count;
     printf("Please enter the number of inputs: ");
