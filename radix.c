f4a458f3f3c26b2e6817a86461c0fb76208d10e3
LEGION-0-1 <the.dilpreet.1@gmail.com>
2024-08-16 20:05:15 +0530

    radix sort program complete

--- radix.c
+++ radix.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
 
+int getMax(int arr[], int n){
+    int mx = arr[0];
+    for (int i = 1; i < n; i++){
+        if (arr[i] > mx){
+            mx = arr[i];
+        }
+    }
+    return mx;
+}
+
+void countSort(int arr[], int n, int exp) {
+    int output[n];
+    int count[10] = {0};
+
+    for (int i = 0; i < n; i++){
+        count[(arr[i] / exp) % 10]++;
+    }
+    for (int i = 1; i < 10; i++){
+        count[i] += count[i - 1];
+    }
+    for (int i = n - 1; i >= 0; i--){
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (int i = 0; i < n; i++){
+        arr[i] = output[i];
+    }
+}
+
+void radixSort(int arr[], int n){  
+    int m = getMax(arr, n); 
+
+    for (int exp = 1; m / exp > 0; exp *= 10){
+        countSort(arr, n, exp);
+    }
+}
+
+void printArray(int arr[], int n){
+    printf("\nThe sorted array is: ");
+    for (int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+}
+
 int main(){
     int a,count;
     printf("Please enter the number of inputs: ");
@@ -13,4 +58,7 @@ int main(){
     for(int i = 0; i < count; i++){
         printf("%d ", num[i]);
     }
+    radixSort(num, count);
+    printArray(num, count);
+    return 0;
 }
