fac4d1afd9b90bf974b611827e9582113836d679
LEGION-0-1 <the.dilpreet.1@gmail.com>
2024-08-22 20:05:09 +0530

    Comments to explain working of bubble sort program

--- Sorting/bubble.c
+++ Sorting/bubble.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 
-int bubble(int num[], int count){
+int bubble(int num[], int count){//function for sorting
     for(int a = 0; a < count; a++){
-        for(int i = 0; i < count; i++){
+        for(int i = 0; i < count; i++){//traversing through the array
         if(num[i]>num[i+1]){
-            int temp = num[i];
+            int temp = num[i];//swapping by values
             num[i] = num[i+1];
             num[i+1] = temp;
         }
     }
     }
     printf("\nThe sorted array is: ");
-    for(int i = 0; i < count; i++){
+    for(int i = 0; i < count; i++){//printing the sorted array
         printf("%d ", num[i]);
     }
 }
 
 int main(){
-    int a,count;
+    int count;//variable for number of elements to be input in the array
     printf("Please enter the number of inputs: ");
-    scanf("%d", &count);
-    int num[count];
+    scanf("%d", &count);//giving count an integer value
+    int num[count];//array declaration
     printf("Please enter the numerical inputs: ");
-    for(int i = 0; i < count; i++){
+    for(int i = 0; i < count; i++){//storing inputs in an array
         scanf("%d", &num[i]);
     }
     printf("The original array is: ");
-    for(int i = 0; i < count; i++){
+    for(int i = 0; i < count; i++){//printing the original array
         printf("%d ", num[i]);
     }
-    bubble(num, count);
+    bubble(num, count);//passing values to another function
 }
 
 /*Algorithm
