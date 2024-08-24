9b32aa96402395f48bf973e2b83704638a5bf230
LEGION-0-1 <the.dilpreet.1@gmail.com>
2024-08-24 22:12:14 +0530

    Comments done for insertion sort

--- Sorting/insertion.c
+++ Sorting/insertion.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
 
-void insertion(int num[], int len)
-{
-    for (int i = 1; i < len; ++i) {
+void insertion(int num[], int len){//function for sorting
+    for (int i = 1; i < len; ++i) {//traversing through the array
         int a = num[i];
         int b = i - 1;
 
-        while (b >= 0 && num[b] > a) {
+        while (b >= 0 && num[b] > a) {//swapping by value 
             num[b + 1] = num[b];
             b = b - 1;
         }
         num[b + 1] = a;
     }
     printf("\nThe sorted array is: ");
-    for (int i = 0; i < len; ++i)
+    for (int i = 0; i < len; ++i)//printing the sorted array
         printf("%d ", num[i]);
 }
 int main(){
-    int a,count;
+    int count;//variable for number of elements to be input in the array
     printf("Please enter the number of inputs: ");
-    scanf("%d", &count);
-    int num[count];
+    scanf("%d", &count);//giving count an integer value
+    int num[count];//array declaration
     printf("Please enter the numerical inputs: ");
-    for(int i = 0; i < count; i++){
+    for(int i = 0; i < count; i++){//storing inputs in an array
         scanf("%d", &num[i]);
     }
     printf("The original array is: ");
-    for(int i = 0; i < count; i++){
+    for(int i = 0; i < count; i++){//printing the original array
         printf("%d ", num[i]);
     }
     int len = sizeof(num)/ sizeof(num[0]);
-    insertion(num, len);
+    insertion(num, len);//passing values to another function
 }
 
 /*Algorithm
